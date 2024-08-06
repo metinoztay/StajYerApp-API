e2a037313ef96e977e2be8bb79efc661a8c136e5
metinoztay <metinoztay@gmail.com>
2024-08-06 12:59:05 +0300

    Advert Controller Updated
    Kullanıcının ilana başvurma bilgisi eklendi

--- Controllers/ApplicationController.cs
+++ Controllers/ApplicationController.cs
@@ -47,6 +47,8 @@ namespace StajYerApp_API.Controllers
         }
         #endregion
 
+        
+
         #region İlana Başvuranları Listeleme
         [HttpGet("ListAdvertsApplications/{advertId}")]
         public async Task<ActionResult> ListAdvertsApplications(int advertId)
