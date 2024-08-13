30439ac29c71739699fbdf7b6aeb4c86c612cc23
metinoztay <metinoztay@gmail.com>
2024-08-13 14:52:21 +0300

    Company Follow controller ına endpoindler eklendi

--- Controllers/CompanyFollowController.cs
+++ Controllers/CompanyFollowController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StajYerApp_API.DTOs;
 using StajYerApp_API.Models;
 
 namespace StajYerApp_API.Controllers
@@ -15,6 +17,72 @@ namespace StajYerApp_API.Controllers
             _context = context;
         }
 
+        #region Kullanıcı için şirket takibi ve takipten çıkma
+        [HttpPost("UserFollowUnfollowCompany")]
+        public async Task<ActionResult> UserFollowUnfollowCompany([FromBody] FollowCompanyModel model)
+        {
+            var follow = _context.UserFollowedCompanies.Where(u => u.UserId == model.UserId && u.CompId == model.CompId).FirstOrDefault();
+            if (follow != null)
+            {
+                _context.UserFollowedCompanies.Remove(follow);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var comp = await _context.Companies.FindAsync(model.CompId);
+                if (comp != null)
+                {
+                    var flw = new UserFollowedCompany
+                    {
+                        UserId = model.UserId,
+                        CompId = model.CompId
+                    };
+
+                    _context.UserFollowedCompanies.Add(flw);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return Ok();
+        }
+        #endregion
+
+        #region Kullanıcının takip ettiği şirketleri Listeleme
+        [HttpGet("ListUsersFollowedCompanies/{userId}")]
+        public async Task<ActionResult> ListUsersFollowedCompanies(int userId)
+        {
+
+            var follows = await _context.UserFollowedCompanies
+                .Where(u => u.UserId == userId)
+                .Select(u => u.Comp)
+                .ToListAsync();
+
+            if (follows == null || !follows.Any())
+            {
+                return NotFound("Kullanıcının takip ettiği şirket bulunmamaktadır");
+            }
+            
+            return Ok(follows);
+        }
+        #endregion
+
+        #region Kullanıcının takip ettiği şirketlerin ilanlarını Listeleme
+        [HttpGet("ListUsersFollowedCompaniesAdverts/{userId}")]
+        public async Task<ActionResult> ListUsersFollowedCompaniesAdverts(int userId)
+        {
+
+            var follows = await _context.UserFollowedCompanies
+                .Where(u => u.UserId == userId)
+                .Select(u => u.Comp).SelectMany(u => u.Advertisements)
+                .ToListAsync();
+
+            if (follows == null || !follows.Any())
+            {
+                return NotFound("Kullanıcının takip ettiği şirket bulunmamaktadır");
+            }
+
+            return Ok(follows);
+        }
+        #endregion
 
     }
 }
---
+++ DTOs/FollowCompanyModel.cs
@@ -0,0 +1,11 @@
+﻿using StajYerApp_API.Models;
+
+namespace StajYerApp_API.DTOs
+{
+    public class FollowCompanyModel
+    {
+        public int UserId { get; set; }
+
+        public int CompId { get; set; }
+    }
+}
